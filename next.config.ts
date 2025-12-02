30fdb033d50087bee3b303257f0f9826c93ad619
Thanathorn-S <159753731+Thanathorn-S@users.noreply.github.com>
2025-12-03 00:58:40 +0700

    rep0.091

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // ✅ Add this to allow CORS from your backend
-  allowedDevOrigins: ["https://dante-demo1.vercel.app", "http://localhost:8080"],
+  allowedDevOrigins: ["https://dante-backend.onrender.com", "http://localhost:8080"],
 };
 
 export default nextConfig;
