e5721b7d85a4d3969f4b10253e7d5987cd53ce50
Thanathorn-S <159753731+Thanathorn-S@users.noreply.github.com>
2025-12-02 23:11:34 +0700

    rep0.04

--- src/backend/route/index.ts
+++ src/backend/route/index.ts
@@ -1,10 +1,10 @@
 import { Router, Request, Response } from 'express';
-import { assistantRoute } from './assistant/index';
+// import { assistantRoute } from './assistant/index';
 import { emoteRoute } from './emote/index';
 import { agentRoute } from './agent/index';
 import { filesRoute } from './files/index';
 import { kpiRoute } from './kpi/index';
-import { vectorRoute } from './vector/index';
+// import { vectorRoute } from './vector/index';
 import { userRoute } from './user/index';
 import { auth0Route } from './auth0/index';
 import { requiresAuth } from 'express-openid-connect';
@@ -15,11 +15,11 @@ export const route = Router();
 // route.use("/emote",emoteRoute);
 // route.use("/agent",agentRoute);
 
-route.use("/assistant",requiresAuth(),assistantRoute);
+// route.use("/assistant",requiresAuth(),assistantRoute);
 route.use("/emote",requiresAuth(),emoteRoute);
 route.use("/agent",requiresAuth(),agentRoute);
 route.use("/files",requiresAuth(),filesRoute);
 route.use("/kpi",requiresAuth(),kpiRoute);
-route.use("/vector",requiresAuth(),vectorRoute);
+// route.use("/vector",requiresAuth(),vectorRoute);
 route.use("/user",requiresAuth(),userRoute);
 route.use("/auth0",requiresAuth(),auth0Route);
